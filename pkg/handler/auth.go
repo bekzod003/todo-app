5a5f1040a88b586c62572f14cd5247efc97464cc
bekzod003 <begiobm@gmail.com>
2021-11-07 20:58:46 +0500

    response.go added, used in auth.go

--- pkg/handler/auth.go
+++ pkg/handler/auth.go
@@ -1,9 +1,19 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	todoapp "todo-app"
+
+	"github.com/gin-gonic/gin"
+)
 
 func (h *Handler) sighUp(c *gin.Context) {
+	var input todoapp.User
 
+	if err := c.BindJSON(&input); err != nil {
+		NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
 }
 
 func (h *Handler) sighIn(c *gin.Context) {
